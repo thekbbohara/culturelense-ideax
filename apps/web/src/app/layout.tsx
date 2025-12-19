1af7f2df8be87e2fc98f640872fe4c5f50913134
Sandip Shrestha <c.restsandip123@gmail.com>
2025-12-19 14:17:33 +0545

    fix some issues, add layouts

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,26 +1,25 @@
-import "../styles/globals.css";
-import type { Metadata } from "next";
+import HomepageLayout from '@/components/HomepageLayout';
+import '../styles/globals.css';
+import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: "CultureLense",
-  description: "Discover cultural entities",
-  manifest: "/manifest.json",
+  title: 'CultureLense',
+  description: 'Discover cultural entities',
+  manifest: '/manifest.json',
 };
 
 export const viewport = {
-  width: "device-width",
+  width: 'device-width',
   initialScale: 1,
   maximumScale: 1,
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <HomepageLayout>{children}</HomepageLayout>
+      </body>
     </html>
   );
 }
